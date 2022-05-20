f83cba92c2a7b9c376c182b2ab8b4857cd66b18c
pablinos <pablinos@users.noreply.github.com>
2022-05-20 21:11:35 +0000

    Components: Add Jetpack Social product icon (#24449)
    * Components: Add Jetpack Social product icon
    
    This adds the new product icon for Jetpack Social to the components
    package.
    
    * Fix package version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2360639776

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8cdeec4cf802e22f7a9363b6e7d045bc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4f2bfac86b73a097d54d0369b29f085c');

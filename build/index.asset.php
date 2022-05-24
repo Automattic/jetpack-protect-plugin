b12a21aff8dcd9d33845959bf67577fe937efee5
samiff <samiff@users.noreply.github.com>
2022-05-24 19:31:14 +0000

    JS Components: add isExternalLink button property (#24475)
    * [not verified] js-components: add isExternalLink prop to Button
    
    * [not verified] add a warning when using external-link variation
    
    * [not verified] changelog
    
    * [not verified] remove warn
    
    * Bump package version
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2380067409

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '463a9be9d6b9c0113264526c6f317fcf');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e042aa6966935403bf384b8000bb609a');

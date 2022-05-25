74f77fe71785210ff47b6e36b83e9464e273cbe7
retrofox <retrofox@users.noreply.github.com>
2022-05-25 12:37:05 +0000

    Protect: Re implement "Your results will be ready soon" layout (#24492)
    * protect: apply 7-5 to Soon admin section
    
    * remove margin top to the Soon title
    
    * protect: changelog
    
    * js-components: clean isCard prop from Dialog cmp
    
    * JS Components: changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2384398313

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7a3ec5743816da7cfdb4e26859d7deac');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '56ec0c1109104a7f10aeea6411e3e7a9');

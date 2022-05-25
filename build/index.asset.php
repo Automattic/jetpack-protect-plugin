9ee805d553825c824de6bd6874c375b0a5346d5c
jeherve <jeherve@users.noreply.github.com>
2022-05-25 09:58:00 +0000

    Components: Update the Jetpack Social icon (#24491)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2383579429

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8c78d59e9357f6eb8eace66d67e96b67');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7a3ec5743816da7cfdb4e26859d7deac');

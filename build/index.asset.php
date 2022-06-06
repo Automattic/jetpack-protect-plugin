646d4dcacc3cf11f779152b167bc133f614c4b30
samiff <samiff@users.noreply.github.com>
2022-06-06 20:32:41 +0000

    JS Component: move Product offer placeholder above button (#24510)
    * [not verified] js-component: define a min height for Alter cmp
    
    * [not verified] show error message above button in ProductOffer
    
    * [not verified] changelog
    
    * Prettier
    
    * Project versions
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2450222535

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'be7d086fd6a12776d1b7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e973ace54301884c4170');

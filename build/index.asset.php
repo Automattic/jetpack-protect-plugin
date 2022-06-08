6a4ee3448f21406375a4999cd7062b1e7301cc60
dkmyta <dkmyta@users.noreply.github.com>
2022-06-08 22:49:06 +0000

    Fix Protect UI footer plan check (#24657)
    * Reverse conditional check for plan so correct footer section content is displayed
    
    * changelog
    
    * Provide a more descriptive changelog comment
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2464771133

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e973ace54301884c4170');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2e07344427d40dfb568d');

cc1161a9e917e1d697c3e45646bc578dcc218aae
anomiex <anomiex@users.noreply.github.com>
2022-06-17 14:24:16 +0000

    js/analytics: Use jest for tests (#24759)
    Convert from js-test-runner/mocha/chai to jest. Add linting of the tests
    too.
    
    Also something was weird with its use of lodash, but it turns out all
    the browsers we support `Object.assign` so we can just drop lodash
    here entirely.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2516016294

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5c8276ab1f26e4ca0d0c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '700af9cd7d1addb60bc0');

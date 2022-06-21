83d5a984a9fcada4bb126968d1f6899b9f3c1b31
anomiex <anomiex@users.noreply.github.com>
2022-06-21 16:52:33 +0000

    js/components: Fix missing imports (#24792)
    Button isn't importing React, but needs to.
    
    ProductOffer is trying to import a "ReactElement" that doesn't exist.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2536909536

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'eb04113ec6aa89b7ff29');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '10e07be9748c0786aa7e');

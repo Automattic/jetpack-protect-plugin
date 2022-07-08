6fd423a16c9a278a89fbcd77f3907eff65fa14c8
sdixon194 <sdixon194@users.noreply.github.com>
2022-07-08 20:42:26 +0000

    js/analytics: Avoid breaking if `_superProps` hasn't been set (#25043)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2638413881

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ca9cf27a374f80825395');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '01854b92c93651eddcda');

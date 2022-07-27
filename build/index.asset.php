61ae0f27f6c262b326a01ebcb781c77b15d25db9
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-27 11:58:28 +0000

    JS Components: Convert icons to TypeScript (#24908)
    * Refactor Icons to Typescript
    
    * Convert Icons stories to TypeScript
    
    * Add changelog
    
    * Fix versions
    
    * Fix import
    
    * Update index.tsx
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2746347795

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fcc79faac160f1f412a3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2ccb885f53d255c00fba');

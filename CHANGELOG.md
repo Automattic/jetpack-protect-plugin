c14c2f4832f807d5ea1d9d3f99e4e24d77ae8948
nateweller <nateweller@users.noreply.github.com>
2022-08-02 16:13:10 +0000

    Protect: Add a default "See all results" label to the mobile navigation button (#25258)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2783510515

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Added
+- Added a default 'See all results' label to the mobile navigation button.
 - Added a spinner to the in progress scan page in admin
 
 ### Changed
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '20c30675740d651c8e05');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ed2850632ca5f9bfdac0');

6f75df21c28df2347f8480840d6ea4d776216bec
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-05 18:30:42 +0000

    JS Components: Convert ThemeProvider component to TypeScript (#25353)
    * Convert ThemeProvider to TypeScript
    
    * Convert ThemeProvider stories to TypeScript
    
    * Add changelog
    
    * Fix story source not loading
    
    * Fix imports
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2805457174

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a2dbed037210fa7718cd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e5079a063597b52ae1a8');

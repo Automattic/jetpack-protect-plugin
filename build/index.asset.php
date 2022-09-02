cc10ab247a5bda15086b7a1e4ab5d0d4f1984693
dhasilva <dhasilva@users.noreply.github.com>
2022-09-02 17:28:09 +0000

    RNA: Add title-medium-semi-bold variant to Text component (#26017)
    * add title-medium-semi-bold variant to Text
    
    * changelog
    
    * add variant dropdown selector on storybook
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2980726381

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd22307d6fbe495bfee6b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a56e4c0b1eb93bca5c1e');

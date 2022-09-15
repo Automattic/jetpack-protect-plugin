855d31306496418c4982c61113a90990b6cf30de
samiff <samiff@users.noreply.github.com>
2022-09-15 22:06:24 +0000

    RNA: Use `Button` at `ActionButton` (#23936)
    * [not verified] js/components: replace core Button by RNA button
    
    * [not verified] update ActionButton styles
    
    * [not verified] set min width for action button
    
    * [not verified] do not set action button height
    
    * [not verified] action-buttom: remove wrapper
    
    * [not verified] changelog
    
    * ActionButton: Update styles to match current approach
    
    * ActionButton: Remove height
    
    * ActionButton: Support --spacing-base var
    
    * Import type React
    
    * Revert "Import type React"
    
    This reverts commit 94d2c03437a617378278f9288cb4e3925e8b8087.
    
    * RNA: Return classes for action-button
    
    * Bump version
    
    Co-authored-by: retrofox <rdsuarez@gmail.com>
    Co-authored-by: oskosk <oskosk@gmail.com>
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3063939758

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c46c708bc16acf30da7d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '100084c5dd32876dce14');

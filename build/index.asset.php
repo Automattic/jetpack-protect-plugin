2ffbbb99c080b2b34af34201271c7cc644f4e19d
dhasilva <dhasilva@users.noreply.github.com>
2022-09-20 18:01:43 +0000

    RNA: Add body-extra-small-bold variant to Text component (#26295)
    * [not verified] add body-extra-small-bold variant to Text component
    
    * [not verified] changelog
    
    * breaking big line
    
    * fixing version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3092284628

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '100084c5dd32876dce14');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8dbf8b774bbf7f07c4be');

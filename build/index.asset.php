bed6450aa43b81bc77c6ee5d9fa7553a43b28543
oskosk <oskosk@users.noreply.github.com>
2022-09-21 16:39:55 +0000

    Update Pricing table design to current version (#26253)
    Co-authored-by: Osk <oskosk@users.noreply.github.com>
    Co-authored-by: oskosk <oskosk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3099446178

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8dbf8b774bbf7f07c4be');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a6e8360e5a9c0a0d6938');

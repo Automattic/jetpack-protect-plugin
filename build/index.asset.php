f2910cd8883f7e514c47e19c153f4d2d5c055e96
oskosk <oskosk@users.noreply.github.com>
2022-10-05 20:43:31 +0000

    JS Package component Product Price: Fix legend prop typo (#26641)
    Co-authored-by: Osk <oskosk@users.noreply.github.com>
    Co-authored-by: oskosk <oskosk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3192505439

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a7509da667544ff67ae0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a13bf6fffc1c06c52231');

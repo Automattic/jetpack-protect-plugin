1b733a8794bb558e409360c0ddb86f1334439fa6
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-20 20:11:28 +0000

    Add tracks events for purchasing items in wp-admin (#26964)
    * Adding Tracks events
    
    * changelog
    
    * Bump versions
    
    Co-authored-by: Jason Moon <jason.moon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3292520922

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '766bb81101434b4e2ffa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f0659adfcb7b2cb9ab24');

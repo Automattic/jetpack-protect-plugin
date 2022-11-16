51d93e2b93f7b805155803371e1dd026e7512cb7
dhasilva <dhasilva@users.noreply.github.com>
2022-11-16 18:08:39 +0000

    RNA: Fix Button style on tertiary variant when disabled (#27449)
    * add aria-disabled property to Button when disabled
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3481823546

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9d766e6e1c6fbf5d907b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e8237a428ef06e6331a3');

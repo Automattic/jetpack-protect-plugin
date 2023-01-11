7a1eeac93e5a83204efc17fe57eb68f5fbc95b76
nateweller <nateweller@users.noreply.github.com>
2023-01-11 16:53:43 +0000

    Protect: Poll for scan status when scanner is idle and no lastChecked value is available (#28275)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3894792417

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,7 @@ This is an alpha version! The changes listed here are not final.
 ### Fixed
 - Fixed grammar.
 - Include required WAF files in production build.
+- Poll for scan status while scanner is provisioning
 - Updated composer.lock
 
 ## 1.2.0-beta - 2022-12-05
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9ea33557abc186b6420b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1adf97e19fc1adb7f4d0');

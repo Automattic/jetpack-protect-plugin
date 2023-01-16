fb6fb0b9b4018927aec394956f61fde1fcc5a638
nateweller <nateweller@users.noreply.github.com>
2023-01-16 18:03:14 +0000

    Fix variable name
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3932814339

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '27fc7a228b353e695e63');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e640beb5bef26d89962f');

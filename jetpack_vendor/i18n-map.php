7039753acb9cfa70e796b31d3aa70e2158325bf9
samiff <samiff@users.noreply.github.com>
2023-02-20 21:47:07 +0000

    Jetpack: backport 11.9-a.5 changelog (#29056)
    * Jetpack: 11.9-a.5 changelog
    
    * Jetpack: init 11.9-a.6
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4227191789

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,47 +6,47 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.17-alpha',
+      'ver' => '0.2.17',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.17.32-alpha',
+      'ver' => '1.17.32',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.14.0-alpha',
+      'ver' => '1.14.0',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.0-alpha',
+      'ver' => '1.51.0',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.8.38-alpha',
+      'ver' => '0.8.38',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.0-alpha',
+      'ver' => '2.3.0',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.8.1-alpha',
+      'ver' => '1.8.1',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.7.11-alpha',
+      'ver' => '2.7.11',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.2.11-alpha',
+      'ver' => '0.2.11',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.2.3-alpha',
+      'ver' => '0.2.3',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.47.2-alpha',
+      'ver' => '1.47.2',
     ),
     'jetpack-transport-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-transport-helper',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-waf' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-waf',
-      'ver' => '0.9.3-alpha',
+      'ver' => '0.9.3',
     ),
   ),
 );

f0ed46b24da95f1d31e3447400121e6e0a42fa21
thingalon <thingalon@users.noreply.github.com>
2023-03-10 01:25:35 +0000

    [Boost] Update copy around Cloud/Critical CSS (#29250)
    * Update text on getting started page
    
    * Update copy for manual critical css section
    
    * Add paragraph to manual critical css status
    
    * Fix module structure
    
    * Move premium CTA inside critical css (manual) module
    
    * Add @automattic/format-currency dependency
    
    * Update premium CTA to include actual pricing
    
    * Update translation comment
    
    * Add premium tooltip and tooltip components
    
    * Update tooltip link event name
    
    * changelog
    
    * Add close button to tooltip
    Update tooltip hide/show logic
    
    * Revert open/close behavior update
    
    * Update PricingTableItem component to allow custom tooltip class names
    
    * Update pricing table tooltips width
    
    * Update tooltips' translation to not include html
    
    * Add styles for manual update tooltip list
    
    * changelog
    
    * Add missing import
    
    * Add another description under Cloud CSS' description
    
    * Remove unnecessary spacing
    
    * Reduce tooltip styles nesting
    
    * Remove pricing utils helper
    
    * Format HTML string to be more pleasant to look at
    
    * Reduce style nesting
    
    * Update cloud css description
    
    * A couple of very minor width tweaks
    
    * Improve code readability
    
    * Simplify variables
    
    * Improve tooltip styles readability
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4380269399

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b9bb051878ccf8c991dd');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '99ef66588f846ff68474');

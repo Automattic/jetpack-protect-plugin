e197df289bb734d07d2353b69b0a96a18395cb2d
nateweller <nateweller@users.noreply.github.com>
2023-03-30 20:57:29 +0000

    Components: Fix layout issue in ContextualUpgradeTrigger (#29818)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4568789255

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b824c7a02dd8e57be68c');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3ab84d4b88e56eed59fa');

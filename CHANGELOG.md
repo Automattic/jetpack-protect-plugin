bcf203f20b9daaa1b146193a60a8ccd647c3dc93
miguelxpn <miguelxpn@users.noreply.github.com>
2023-04-07 03:40:57 +0000

    [not verified] Protect: Fix firewall status to be considered "on" if brute force protection is enabled (#29853)
    * Consider the firewall 'on' when brute force protection is enabled.
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4635143679

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 1.4.1-alpha - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
 ## 1.4.0-beta - 2023-03-29
 ### Added
 - Add brute force protection. [#28401]
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b824c7a02dd8e57be68c');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f5a19d5afbdcef2dfd58');

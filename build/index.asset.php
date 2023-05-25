14a806ba19550656215171a82bec0e29e4899e1f
pablinos <pablinos@users.noreply.github.com>
2023-05-25 06:06:12 +0000

    Social: Add the instagram social icon (#30803)
    * [not verified] Social: Add the instagram social icon
    
    With an icon available for instagram the connections should be displayed
    correctly in the editor sidebar.
    
    * [not verified] Update to use `instagram-business` as the service name
    
    * Added Publicize changelog
    
    * Update icon and service name.
    
    * Update the Instagram service label.
    
    * Fix the project versions
    
    * Changelog
    
    * Remove duplicate definition of InstagramIcon
    
    ---------
    
    Co-authored-by: spsiddarthan <siddarthan@outlook.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5076526866

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a1ebd9aabf413dc33b99');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9b202864ca8f86baa1ea');

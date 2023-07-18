a5875c047de8e961ff156847e145914f1a09437f
nateweller <nateweller@users.noreply.github.com>
2023-07-18 18:44:45 +0000

    Protect: Update Firewall Copy (#31927)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5591009885

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c973356c4d8a58c56cde');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bfdaf6a9975c2a9bc626');

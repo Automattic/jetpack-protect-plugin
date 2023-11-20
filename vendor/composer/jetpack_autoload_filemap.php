ca0bd5cb4255f282571572cf0c96dd3940bde4a0
jeherve <jeherve@users.noreply.github.com>
2023-11-20 15:34:25 +0000

    Code Modernization: Replace strpos() with str_starts_with() (#34135)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6932111860

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,7 +7,7 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '2.0.0.0-alpha1700485793',
+		'version' => '2.0.0.0-alpha1700493473',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'590147edb63acb03b2852d5c4db13ce8' => array(

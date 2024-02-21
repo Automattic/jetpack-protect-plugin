a76b44a149b309d18dff737943525c39eee2bab3
monsieur-z <monsieur-z@users.noreply.github.com>
2024-02-21 14:20:47 +0000

    Jetpack Logo: prevent VoiceOver on Safari from reading SVG content (#35752)
    * Jetpack Logo: prevent VoiceOver on Safari from reading SVG content
    
    * Update test snapshot
    
    * Fix package version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7990316819

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd650aee81c56e531fc92');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '87548f5432d9fbdf5459');

47c704063875604d937381da6d0e8481b0452f27
nateweller <nateweller@users.noreply.github.com>
2024-07-07 21:30:50 +0000

    Protect: Add "routes" directory (#37941)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9830370178
    
    Upstream-Ref: Automattic/jetpack@4e70a30678da8de9a2bcd0ac0238f4bcbfac54cd

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e4b9b93f669c0e056e29');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e917452377571a97b8b8');

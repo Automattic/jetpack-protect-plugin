caafa44fdf84cedc86b8d116a06e3e6190921e12
dkmyta <dkmyta@users.noreply.github.com>
2024-08-14 19:11:14 +0000

    Hide IP list sub-settings when empty and disabled (#38849)
    changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10393378597
    
    Upstream-Ref: Automattic/jetpack@2b8a5ebbe1fdef1ef4da0c3e5fad9c715af93678

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '158f413b9e798c8df589');
+<?php return array('dependencies' => array('jetpack-script-data', 'moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9caa3fd4b0ac0b36b68a');

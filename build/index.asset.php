d0bf4f8fd07d9346d09ba2593e56e8c2039019d0
nateweller <nateweller@users.noreply.github.com>
2024-08-18 17:31:44 +0000

    Protect: Fix manual scans section link button loading state (#38897)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10442410391
    
    Upstream-Ref: Automattic/jetpack@3224c19aad91a959d5f04eb5e0613d6e46618443

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'moment', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8b6fcb19acc4a7fd42ad');
+<?php return array('dependencies' => array('jetpack-script-data', 'moment', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cc638f200ea04f5dcac9');

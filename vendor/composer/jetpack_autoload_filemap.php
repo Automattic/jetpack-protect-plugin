facca60bbf4e6755c704968e5f0dfefd07e2f167
ice9js <ice9js@users.noreply.github.com>
2024-09-23 20:22:49 +0000

    Changelog and readme.txt edits. (#39497)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11001614719
    
    Upstream-Ref: Automattic/jetpack@89f781260f91d4acff0467d33b4a8242a7f2de37

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,15 +11,15 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '5.0.0.0-alpha1727102804',
+		'version' => '5.0.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'590147edb63acb03b2852d5c4db13ce8' => array(
-		'version' => '0.2.4.0',
+		'version' => '0.2.5.0-alpha1727122113',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-transport-helper/actions.php'
 	),
 	'3d45c7e6a7f0e71849e33afe4b3b3ede' => array(
-		'version' => '0.19.0.0-alpha1726864285',
+		'version' => '0.19.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-waf/cli.php'
 	),
 );

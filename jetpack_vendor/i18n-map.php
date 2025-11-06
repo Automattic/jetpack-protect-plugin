70d5282bdf13936f691a9b4d8f8e0a0602e3d269
coder-karen <coder-karen@users.noreply.github.com>
2025-11-06 10:26:01 +0000

    Jetpack Sync: Do not sync shop_order_placehold post-type (#45772)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19132093107
    
    Upstream-Ref: Automattic/jetpack@28738f1d2551f640d6527d8245f8ff172a61d8b7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.22.4-alpha1762349303',
+      'ver' => '4.22.4-alpha1762423778',
     ),
     'jetpack-transport-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-transport-helper',

d568f1d1c1fd974f56fcdbf58adbff35d907712e
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-12-22 10:53:27 +0000

    Backport jetpack 15.4-a.5 changes (#46383)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Fix typo
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20429384995
    
    Upstream-Ref: Automattic/jetpack@d30c7fd4782e236aa8f4624b6c892b4bcefc14e0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.18-alpha1766165482',
+      'ver' => '4.3.18',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.13-alpha1766380116',
+      'ver' => '6.19.13',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.4-alpha1766380116',
+      'ver' => '0.4.4',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.13-alpha1766380116',
+      'ver' => '4.3.13',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.29.0-alpha1766092444',
+      'ver' => '5.29.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.24.2-alpha1765963715',
+      'ver' => '4.24.2',
     ),
     'jetpack-transport-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-transport-helper',

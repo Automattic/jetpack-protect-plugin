fe98cbe5116f6725e128c365db6b8f4f5d2b8b8c
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-02-23 14:04:15 +0000

    Backport jetpack 15.6-a.5 changes (#47272)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22308695085
    
    Upstream-Ref: Automattic/jetpack@deee48f8d6281794917538bee6827fe7cdb6b370

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,15 +11,15 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.0.0.0-alpha1771601476',
+		'version' => '8.0.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'590147edb63acb03b2852d5c4db13ce8' => array(
-		'version' => '0.3.4.0',
+		'version' => '0.3.5.0-alpha1771854229',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-transport-helper/actions.php'
 	),
 	'3d45c7e6a7f0e71849e33afe4b3b3ede' => array(
-		'version' => '0.28.0.0',
+		'version' => '0.28.1.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-waf/cli.php'
 	),
 );

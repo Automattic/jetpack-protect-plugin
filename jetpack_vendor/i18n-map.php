82ff17083d3e5035a9c493de1c24260e06df7636
anomiex <anomiex@users.noreply.github.com>
2026-09-18 15:56:03 +0000

    Update dependency date-fns to v4.4.0 (#52402)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35363274694
    
    Upstream-Ref: Automattic/jetpack@80530666d2b83914ce7de584d6ba50a6b904c956

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.4.0-alpha1789743694',
+      'ver' => '0.4.0-alpha1789745679',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
